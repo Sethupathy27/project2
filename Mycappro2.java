61c07f5adc3090e931edca95cd0b4d35e65415e0
Sethupathy27 <83457122+Sethupathy27@users.noreply.github.com>
2021-05-20 14:22:31 +0530

    project2.java
    This file prints convert minutes into a number of years and days

---
+++ Mycappro2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author sethu
+ */
+public class Mycappro2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int min,hr,days,years;
+		System.out.println("enter the number of mintues");
+		Scanner m=new Scanner(System.in);
+		min=m.nextInt();
+		hr=min/60;
+		days=hr/24;
+		years=days/365;
+		System.out.println("Number of days : "+days);
+		System.out.println("Numner of years: "+years);
+        // TODO code application logic here
+    }
+    
+}
